ac82c0f714c7c39df4428f44b2cabc693885ee47
Irene Mutegi <irene@Ms-Echo.>
2024-10-01 12:22:00 +0300

    array methods

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tutorials = [
+const tutorials = [ 
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
@@ -12,5 +12,15 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(" ") // Split into words
+      .map(word => {
+        // Capitalize the first letter and keep the rest as is
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(" "); // Join words back into a sentence
+  });
+};
+
+console.log(titleCased());
